863e787e137f135be116ce5cff524dfc579182da
Khalid <khalidbkr@gmail.com>
2023-05-28 23:20:15 +0100

    Update 280523_Heathrow_passengers_forcast.py

--- Heathrow passengers forcast/280523_Heathrow_passengers_forcast.py
+++ Heathrow passengers forcast/280523_Heathrow_passengers_forcast.py
@@ -4,29 +4,29 @@
 # Months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
 # Passengers_millions = [3.789,3.530,3.570,3.131,3.221,3.602,4.082,4.325,4.091,4.244,3.891,3.607]
 
-# import matplotlib.pyplot as pyplot  
-# # IDENTIFYING SUMMER PERIOD
-# summer_m = [Months[5], Months[6], Months[7]]
-# print(summer_m)
-# # MATCHING MONTH INDEX TO PASSENGER NUMBERS
-# summerPass_number = [Passengers_millions[5], Passengers_millions[6], Passengers_millions[7]]
-# print(summerPass_number)
-# # CALCULATING AVERAGE PASSENGER NUMBER
-# avg_pass_number_summer = sum(summerPass_number) / 3
-# print(avg_pass_number_summer)
-# # WINTER PERIOD
-# winter_m = [Months[9], Months[10], Months[11]]
-# print(winter_m)
-# winterPass_number = [Passengers_millions[9], Passengers_millions[10], Passengers_millions[11]]
-# print(winterPass_number)
-# avg_pass_number_winter = sum(winterPass_number) / 3
-# print(avg_pass_number_winter)
-# # CREATE VARIABLES FOR GRAPH AXISES
-# seasons = ['winter', 'summer']
-# values = [avg_pass_number_summer, avg_pass_number_winter]
-# # PLOT CUSTOMIZATION
-# pyplot.barh(seasons, values)
-# pyplot.show()
+import matplotlib.pyplot as pyplot  
+# IDENTIFYING SUMMER PERIOD
+summer_m = [Months[5], Months[6], Months[7]]
+print(summer_m)
+# MATCHING MONTH INDEX TO PASSENGER NUMBERS
+summerPass_number = [Passengers_millions[5], Passengers_millions[6], Passengers_millions[7]]
+print(summerPass_number)
+# CALCULATING AVERAGE PASSENGER NUMBER
+avg_pass_number_summer = sum(summerPass_number) / 3
+print(avg_pass_number_summer)
+# WINTER PERIOD
+winter_m = [Months[9], Months[10], Months[11]]
+print(winter_m)
+winterPass_number = [Passengers_millions[9], Passengers_millions[10], Passengers_millions[11]]
+print(winterPass_number)
+avg_pass_number_winter = sum(winterPass_number) / 3
+print(avg_pass_number_winter)
+# CREATE VARIABLES FOR GRAPH AXISES
+seasons = ['winter', 'summer']
+values = [avg_pass_number_summer, avg_pass_number_winter]
+# PLOT CUSTOMIZATION
+pyplot.barh(seasons, values)
+pyplot.show()
 
 
 
@@ -40,18 +40,18 @@
 # Months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
 # Passengers_millions = [3.789,3.530,3.570,3.131,3.221,3.602,4.082,4.325,4.091,4.244,3.891,3.607]
 
-# import numpy as numpy
-# import matplotlib.pyplot as pyplot
-# # CALCULATING INCREASE OF 9% FOR NEXT YEAR
-# NxtYear_forcast = numpy.array(Passengers_millions) * 1.09
-# print (NxtYear_forcast)
-# # APPLYING THE FORCAST INCREASE ONLY FOR SUMMER PERIOD
-# NxtSummer_forcast = numpy.mean(NxtYear_forcast[5:8])
-# print(NxtSummer_forcast)
-# pyplot.plot(Months, Passengers_millions, label = 'Current year')
-# pyplot.plot(Months, NxtYear_forcast, label = 'next year')
-# pyplot.legend()
-# pyplot.show()
+import numpy as numpy
+import matplotlib.pyplot as pyplot
+# CALCULATING INCREASE OF 9% FOR NEXT YEAR
+NxtYear_forcast = numpy.array(Passengers_millions) * 1.09
+print (NxtYear_forcast)
+# APPLYING THE FORCAST INCREASE ONLY FOR SUMMER PERIOD
+NxtSummer_forcast = numpy.mean(NxtYear_forcast[5:8])
+print(NxtSummer_forcast)
+pyplot.plot(Months, Passengers_millions, label = 'Current year')
+pyplot.plot(Months, NxtYear_forcast, label = 'next year')
+pyplot.legend()
+pyplot.show()
 
 
 
